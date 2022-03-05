4a81b63a4cb6d893fd0f4285ce8f6191e42d7dc9
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-03-05 22:42:02 +0530

    functions_js

---
+++ functions.js
@@ -0,0 +1,26 @@
+function sayHello(){
+    console.log("Hello from functions");
+}
+
+sayHello();
+
+//funtions with parameter
+
+function sum(num1, num2){
+    let addition = num1+num2;
+    console.log("Addition of given number is "+addition);
+}
+sum(3,5);
+
+function multiply(num1,num2){
+    return num1*num2;
+}
+
+let ans = multiply(3,5);
+console.log(ans);
+
+//Storing function in a variable
+let a = function sub(num1,num2){
+    return num1-num2;
+}
+console.log(a(3,5));
